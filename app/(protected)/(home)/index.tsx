7e721abd69c1eb1554761838435b61923439e5df
PratikDhungel <pratikdhungel7.0@gmail.com>
2025-08-05 18:53:51 +0545

    Show more stats in summary tab using mock data

--- app/(protected)/(home)/index.tsx
+++ app/(protected)/(home)/index.tsx
@@ -5,7 +5,7 @@ import ScreenWrapper from '@/components/common/ScreenWrapper'
 export default function Home() {
   return (
     <ScreenWrapper>
-      <View style={{ display: 'flex', flexDirection: 'row', gap: 12 }}>
+      <View style={{ display: 'flex', flexDirection: 'row', gap: 12, marginBottom: 12 }}>
         <View
           style={{
             padding: 12,
@@ -16,7 +16,7 @@ export default function Home() {
             alignItems: 'center',
           }}
         >
-          <Text style={{ fontSize: 16, marginBottom: 12 }}>Total Rentals</Text>
+          <Text style={{ fontSize: 16, marginBottom: 12, fontWeight: 500 }}>Total Rentals</Text>
 
           <View
             style={{
@@ -44,7 +44,7 @@ export default function Home() {
             alignItems: 'center',
           }}
         >
-          <Text style={{ fontSize: 16, marginBottom: 12 }}>Total Earnings</Text>
+          <Text style={{ fontSize: 16, marginBottom: 12, fontWeight: 500 }}>Total Earnings</Text>
 
           <View
             style={{
@@ -59,8 +59,96 @@ export default function Home() {
             }}
           >
             <Text style={{ fontSize: 16, maxWidth: 60 }} ellipsizeMode="tail" numberOfLines={1}>
-              10000000
+              50758
             </Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={{ display: 'flex', flexDirection: 'row', gap: 12, marginBottom: 12 }}>
+        <View
+          style={{
+            padding: 12,
+            borderRadius: 20,
+            backgroundColor: '#fff',
+            flex: 1,
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <Text style={{ fontSize: 16, marginBottom: 12, fontWeight: 500 }}>Owned Rentals</Text>
+
+          <View
+            style={{
+              borderWidth: 8,
+              borderRadius: '50%',
+              borderColor: 'red',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: 100,
+              height: 100,
+            }}
+          >
+            <Text style={{ fontSize: 16, maxWidth: 60 }} ellipsizeMode="tail" numberOfLines={1}>
+              4
+            </Text>
+          </View>
+        </View>
+
+        <View
+          style={{
+            padding: 12,
+            borderRadius: 20,
+            backgroundColor: '#fff',
+            flex: 1,
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <Text style={{ fontSize: 16, marginBottom: 12, fontWeight: 500 }}>Total Expenditure</Text>
+
+          <View
+            style={{
+              borderWidth: 8,
+              borderRadius: '50%',
+              borderColor: 'red',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: 100,
+              height: 100,
+            }}
+          >
+            <Text style={{ fontSize: 16, maxWidth: 60 }} ellipsizeMode="tail" numberOfLines={1}>
+              10973
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={{ display: 'flex', flexDirection: 'row', gap: 12, marginBottom: 12 }}>
+        <View
+          style={{
+            padding: 12,
+            borderRadius: 20,
+            backgroundColor: '#fff',
+            flex: 1,
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <Text style={{ fontSize: 16, marginBottom: 12, fontWeight: 500 }}>Earnings by month</Text>
+
+          <View
+            style={{
+              height: 200,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Text style={{ fontSize: 24 }}>Show bar graphs here</Text>
           </View>
         </View>
       </View>
