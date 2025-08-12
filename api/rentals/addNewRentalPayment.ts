acf9dde59a818595cb5a579996c6c4da933d6c62
PratikDhungel <pratikdhungel7.0@gmail.com>
2025-08-12 17:11:17 +0545

    Create new file the handle new rental payment request

---
+++ api/rentals/addNewRentalPayment.ts
@@ -0,0 +1,11 @@
+import { api } from '@/utils/axios'
+
+interface IAddNewRentalPaymentVariables {
+  rental_id: string
+  amount: number
+  payment_date: string
+}
+
+export function addNewRental(addRentalPaymentVariables: IAddNewRentalPaymentVariables) {
+  return api.post('/rentals/create-rental-payment', addRentalPaymentVariables)
+}
