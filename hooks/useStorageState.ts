2fb235cfcdc7521ccad49c7250502d769075bc83
PratikDhungel <pratikdhungel7.0@gmail.com>
2025-08-13 14:12:38 +0545

    Add comment to useStorageState reducer function

--- hooks/useStorageState.ts
+++ hooks/useStorageState.ts
@@ -1,12 +1,22 @@
 import { useEffect, useCallback, useReducer } from 'react'
 import { getStorageItemAsync, setStorageItemAsync, storageKey } from '@/utils/storage'
 
+// async state tuple with [loading, storeValue]
 type TAsyncState<T> = [boolean, T | null]
 
+// useState like tuple for async store value with state and reducer function
 type UseStateHook<T> = [TAsyncState<T>, (value: T | null) => void]
 
+// after async store fetch update state with value, set loading to false
+function asyncStateReducerFunction<T>(
+  state: TAsyncState<T>,
+  action: T | null = null,
+): TAsyncState<T> {
+  return [false, action]
+}
+
 function useAsyncState<T>(initialValue: TAsyncState<T> = [true, null]): UseStateHook<T> {
-  return useReducer((_, action: T | null = null): TAsyncState<T> => [false, action], initialValue)
+  return useReducer(asyncStateReducerFunction<T>, initialValue)
 }
 
 export function useStorageState(key: storageKey): UseStateHook<string> {
